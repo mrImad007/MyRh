b55da6126fbde56a9c379af22d8377dab8518559
mrImad007 <imadeddinzaoui@gmail.com>
2023-12-21 16:33:36 +0100

    Strating the project Architectural Pattern

---
+++ src/main/java/com/project/MyRh/MyRhApplication.java
@@ -0,0 +1,13 @@
+package com.project.MyRh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRhApplication.class, args);
+	}
+
+}
