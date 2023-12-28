c477c1ad8dfa63ec50f15d3556b0e31bd124dca3
mrImad007 <imadeddinzaoui@gmail.com>
2023-12-28 14:10:50 +0100

    cleaning

--- src/main/java/com/project/MyRh/Repositories/CompanyRepository.java
+++ src/main/java/com/project/MyRh/Repositories/CompanyRepository.java
@@ -1,9 +1,7 @@
 package com.project.MyRh.Repositories;
 
-import com.project.MyRh.DTO.CompanyDto;
 import com.project.MyRh.Models.Entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
